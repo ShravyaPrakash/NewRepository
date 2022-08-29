6eef9985cdf45bbcaa9b4bb47fd6103f69f2450d
ShravyaPrakash <70698616+ShravyaPrakash@users.noreply.github.com>
2022-08-29 15:27:09 +0530

    Add files via upload

---
+++ Aug26_HandsOn/Problem1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public struct Student
+{
+
+    public int RollNo;
+    public string Name;
+    public int Percentage;
+    public void SetStudent(int Rollno, string name, int percent)
+    {
+        RollNo = Rollno;
+        Name = name;
+        Percentage = percent;
+    }
+
+    public void DisplayStudent()
+    {
+        Console.WriteLine("Student Details:");
+        Console.WriteLine("\tRoll Number : " + RollNo);
+        Console.WriteLine("\tName : " + Name);
+        Console.WriteLine("\tPercentage : " + Percentage);
+        Console.WriteLine("\n");
+    }
+}
+
+public class Problem1
+{
+
+    static void Main(string[] args)
+    {
+
+        Student[] std = { new Student(), new Student(), new Student() };
+
+        std[0].SetStudent(1, "AAA", 95);
+        std[1].SetStudent(2, "BBB", 94);
+        std[2].SetStudent(3, "CCC", 96);
+
+
+        std[0].DisplayStudent();
+        std[1].DisplayStudent();
+        std[2].DisplayStudent();
+    }
+}
---
+++ Aug26_HandsOn/Problem2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+    class Students
+    {
+        private int rollno;
+        private string name;
+        private int age;
+
+        public void SetInfo(string name, int rollno, int age)
+        {
+            this.rollno = rollno;
+            this.age = age;
+            this.name = name;
+        }
+
+        public void printInfo()
+        {
+            Console.WriteLine("Student Record: ");
+            Console.WriteLine("\tName     : " + name);
+            Console.WriteLine("\tRollNo   : " + rollno);
+            Console.WriteLine("\tAge      : " + age);
+        }
+
+    }
+
+    class Problem2
+    {
+        static void Main()
+        {
+            Students[] S = new Students[2];
+            S[0] = new Students();
+            S[1] = new Students();
+            S[0].SetInfo("Shravya", 101, 25);
+            S[0].printInfo();
+            S[1].SetInfo("Prakash", 102, 27);
+            S[1].printInfo();
+
+        }
+    }
