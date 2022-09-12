faf6dab99bcca2c1931143258ad3c8bd7f677cc7
ShravyaPrakash <70698616+ShravyaPrakash@users.noreply.github.com>
2022-09-12 09:06:00 +0530

    Add files via upload

---
+++ Sept8_HandsOn/ClassLibrary.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public delegate void Action_edit_update_del(int empid, Department dd);
+
+    public delegate void Action_add();
+    public class Department
+    {
+        public event Action_edit_update_del UpdateElist;
+
+        public event Action_add AddEvent;
+        private int _deptid;
+
+        public int DeprtmentID
+        {
+            get { return _deptid; }
+            set { _deptid = value; }
+        }
+
+        private string _deptname;
+
+        public string DepartmentName
+        {
+            get { return _deptname; }
+            set { _deptname = value; }
+        }
+
+        private string _location;
+
+        public string Location
+        {
+            get { return _location; }
+            set { _location = value; }
+        }
+
+        private List<Employee> _elist = new List<Employee>();
+
+        public List<Employee> Elist
+        {
+
+            get { return _elist; }
+
+            set
+            {
+                if (Elist.Count <= 10) _elist = value;
+
+                else throw new Exception("Employee in a department should not exceed 10");
+
+            }
+
+        }
+
+        public void getdetails()
+        {
+            Console.WriteLine("Enter Dept Id");
+            this.DeprtmentID = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Dept Name");
+            this.DepartmentName = Console.ReadLine();
+
+            Console.WriteLine("Enter Dept location");
+            this.Location = Console.ReadLine();
+
+
+        }
+
+        public void showmenu()
+        {
+            Console.WriteLine("******************************************");
+
+            Console.WriteLine("MENU \n1  - Add Employee from dept\n2 - Update Employee from dept\n3 - Delete Employee from dept");
+            Console.WriteLine("******************************************");
+
+        }
+        //------------add----------------
+        public void Add()
+        {
+
+            AddEvent();
+
+        }
+
+        public void Department_AddEvent()
+        {
+            Console.WriteLine("*********************************");
+            Employee emp = new Employee();
+
+            Console.WriteLine("Enter Employee Id");
+            emp.EmployeeId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee Name");
+            emp.EmployeeName = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee dept no");
+            emp.DepartmantNO = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter Employee salary");
+            emp.Salary = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter manager reporting status");
+            emp.Manager = bool.Parse(Console.ReadLine());
+
+            Elist.Add(emp);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Employee added successfully in list......");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+
+        //------------------------------------
+
+        //edit
+        public void Edit()
+        {
+
+            Console.WriteLine("------EDIT OPERATION--------");
+            Console.WriteLine("Enter Emplyee id to edit name ");
+            int id = int.Parse(Console.ReadLine());
+
+            UpdateElist(id, this);
+        }
+        //---------remove--------------------------------
+
+        public void RemoveEmp()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("------Remove OPERATION--------");
+            Console.WriteLine("Enter Emplyee id to REMOVE ");
+            int id = int.Parse(Console.ReadLine());
+
+            UpdateElist(id, this);
+
+        }
+
+    }
+
+
+    public class Employee
+    {
+        private int _empid;
+
+        public int EmployeeId
+        {
+            get { return _empid; }
+            set { _empid = value; }
+        }
+
+        private string _empName;
+
+        public string EmployeeName
+        {
+            get { return _empName; }
+            set { _empName = value; }
+        }
+
+        private int _deptno;
+
+        public int DepartmantNO
+        {
+            get { return _deptno; }
+            set { _deptno = value; }
+        }
+
+        private int _salary;
+
+        public int Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+
+        private bool _manager;
+
+        public bool Manager
+        {
+            get { return _manager; }
+            set { _manager = value; }
+        }
+
+    }
+}
---
+++ Sept8_HandsOn/Program.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace Sept8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============================");
+            Authenticate();
+
+            Console.WriteLine("==============================");
+
+
+
+            Department d = new Department();
+
+            d.getdetails();
+
+            d.showmenu();
+
+
+            Console.WriteLine("Enter choice");
+            int ch = int.Parse(Console.ReadLine());
+            switch (ch)
+            {
+
+                case 1: //ADD
+                    d.AddEvent += d.Department_AddEvent;
+                    Console.WriteLine("Enter no of employess to be added in dlist(not>10)");
+                    int n1 = int.Parse(Console.ReadLine());
+                    if (n1 <= 10)
+                    {
+                        for (int i = 0; i < n1; i++) { d.Add(); }
+                    }
+                    else
+                    {
+                        throw new Exception("no of employees in dept should not exceed 10");
+                    }
+                    break;
+
+                case 2: // UPDATE OR EDIT
+                    d.AddEvent += d.Department_AddEvent;
+                    Console.WriteLine("Enter no of employess to be added in dlist");
+                    int n2 = int.Parse(Console.ReadLine());
+                    if (n2 <= 10)
+                    {
+                        for (int i = 0; i < n2; i++) { d.Add(); }
+                    }
+                    else
+                    {
+                        throw new Exception("no of employees in dept should not exceed 10");
+                    }
+                    printlist(d);
+                    d.UpdateElist += Editlist;
+                    d.Edit();
+                    break;
+                case 3: // REMOVE
+                    d.AddEvent += d.Department_AddEvent;
+                    Console.WriteLine("Enter no of employess to be added in dlist");
+                    int n3 = int.Parse(Console.ReadLine());
+                    if (n3 <= 10)
+                    {
+                        for (int i = 0; i < n3; i++) { d.Add(); }
+                    }
+                    else
+                    {
+                        throw new Exception("no of employees in dept should not exceed 10");
+                    }
+                    printlist(d);
+                    d.UpdateElist += Remove;
+                    d.RemoveEmp();
+                    break;
+            }
+
+            //Delegates - empdetails | Mgr report status true list
+
+            Empshowdetails(d);
+
+            MgrReporting(d);
+
+            //General print list
+            printlist(d);
+
+            Console.ReadLine();
+
+
+        } //main() end
+
+
+        private static void MgrReporting(Department d)
+        {
+            Console.WriteLine("Manager report: true status list");
+
+            List<Employee> mgrlist = d.Elist.FindAll(x => x.Manager == true);
+
+
+            Action<List<Employee>> mrep = mlist =>
+            {
+                foreach (var item in mgrlist)
+                {
+                    Console.WriteLine("-------------------------------------");
+                    Console.WriteLine($"Name    : {item.EmployeeName}");
+                    Console.WriteLine($"EmpID   : {item.EmployeeId}");
+                    Console.WriteLine($"DeptNo  : {item.DepartmantNO}");
+                    Console.WriteLine($"Salary  : {item.Salary}");
+                    Console.WriteLine($"Manager status : {item.Manager}");
+                    Console.WriteLine("-------------------------------------");
+                }
+            };
+
+            mrep(mgrlist);
+            Console.WriteLine("=====================================================");
+
+        }
+        private static void Empshowdetails(Department d)
+        {
+            Console.WriteLine("Employee and dept details using delegates - action");
+
+            Action<Department> deptdetails = dep =>
+            {
+                Console.WriteLine($"Department ID : {dep.DeprtmentID}");
+                Console.WriteLine($"Department Name : {dep.DepartmentName}");
+                Console.WriteLine($"Department Location: {dep.Location}");
+            };
+
+            deptdetails(d);
+
+
+            Action<Department> empshow = obj =>
+            {
+                Console.WriteLine($"Department ID   : {obj.DeprtmentID}");
+                Console.WriteLine($"Department Name : {obj.DepartmentName}");
+                Console.WriteLine($"Department Location: {obj.Location}");
+
+                foreach (var item in obj.Elist)
+                {
+                    Console.WriteLine("-------------------------------------");
+                    Console.WriteLine($"Name    : {item.EmployeeName}");
+                    Console.WriteLine($"EmpID   : {item.EmployeeId}");
+                    Console.WriteLine($"DeptNo  : {item.DepartmantNO}");
+                    Console.WriteLine($"Salary  : {item.Salary}");
+                    Console.WriteLine($"Manager status : {item.Manager}");
+                    Console.WriteLine("-------------------------------------");
+                }
+
+            };
+            empshow(d);
+
+        }
+
+        private static void Authenticate()
+        {
+            Func<string, string, bool> auth = (Userid, password) =>
+            {
+                if (Userid != string.Empty && password != string.Empty &&
+                   Userid == "Durgesh" && password == "Hello@123")
+                {
+                    return true;
+                }
+                else
+                {
+
+                    Console.WriteLine("Retry userid/password");
+
+                    return false;
+
+                }
+
+            };
+            Console.WriteLine("Enter userID");
+            string _userid = Console.ReadLine();
+
+            Console.WriteLine("Enter Password");
+            string _password = Console.ReadLine();
+
+            if (auth(_userid, _password))
+            {
+
+
+                Console.WriteLine("Logged in Successfully");
+
+            }
+            else
+            {
+                Environment.Exit(2);
+            }
+        }
+
+        private static void printlist(Department d)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("-----------Printing List-----------");
+            foreach (var item in d.Elist)
+            {
+                Console.WriteLine(item.EmployeeId);
+                Console.WriteLine(item.EmployeeName);
+                Console.WriteLine(item.DepartmantNO);
+                Console.WriteLine(item.Salary);
+                Console.WriteLine(item.Manager);
+                Console.WriteLine("----------------------");
+            }
+        }
+
+        public static void Editlist(int id, Department dd)
+        {
+            Employee e = dd.Elist.Find(x => x.EmployeeId == id);
+
+
+            Console.WriteLine("Edit Employee Id");
+            e.EmployeeId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Edit Employee Name");
+            e.EmployeeName = Console.ReadLine();
+
+            Console.WriteLine("Edit Employee dept no");
+            e.DepartmantNO = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Edit Employee salary");
+            e.Salary = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Edit manager reporting status");
+            e.Manager = bool.Parse(Console.ReadLine());
+
+        }
+
+
+
+        public static void Remove(int id, Department dd)
+        {
+            Employee rem = dd.Elist.Find(x => x.EmployeeId == id);
+
+            dd.Elist.Remove(rem);
+
+            Console.WriteLine("Removed successfully");
+
+
+        }
+    }
+}
